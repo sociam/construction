2f1d2aefc373d152dc79df3a3880278d25fd2e04
electronic Max <electronic@gmail.com>
2013-03-11 11:14:41 -0700

    app coming together

--- html/js/index.js
+++ html/js/index.js
@@ -17,28 +17,53 @@ var setRating = function() {
 function ElicitationController($scope) {
 
 	$scope.elicitationid = 'elicitation-' + (new Date	()).valueOf();
-	window.eid = $scope.elicitationid;
+	$scope.username = localStorage.username || 'expert-' + (new Date ()).valueOf();
 	$scope.constructs = [];	
 	$scope.element = undefined;
 	$scope.elements = [];
+
 	$scope.elicitation_obj = undefined;
 	$scope.loading = 0;
 
+	$scope.error = '';
+	$scope.error_header = 'uh oh!';
+
+	var done_element_ids = (localStorage.done_elements && JSON.parse(localStorage.done_elements)) || [];
+	
+	var show_error = function(msg) {
+		$scope.$apply(function() {
+			$scope.error = msg;
+			$(".alert").show();
+		});
+	};	
 	var start_loading = function() { $scope.$apply(function() { $scope.loading++; }); };
 	var end_loading = function() { $scope.$apply(function() { $scope.loading--; }); };
-
+	
+	$scope.setUser = function() {
+		localStorage.username = $scope.username;
+		$scope.elicitation_obj.set('expert', $scope.username);
+		delete localStorage.done_elements;
+	};
 	$scope.setRating = function(construct, val) {
 		var eo = $scope.elicitation_obj;
-		console.log('set rating called ', construct, val);
 		eo.set(construct, val);
 		$scope.loading++;
 		eo.save().then(function() {
 			console.log('done!');
 			end_loading();
 		}).fail(function(err) {
-			u.error(err);
+			var s = err.message || JSON.stringify(err);
+			u.error(s);
+			show_error(s);
 			end_loading();
 		});
+	};
+	
+	$scope.nextElement = function() {
+		$scope.loading++;
+		done_element_ids.push($scope.element.id);
+		localStorage.done_elements = JSON.stringify(done_element_ids);
+		window.location.reload();
 	};
 	
 	WebBox.load().then(function() {
@@ -64,14 +89,21 @@ function ElicitationController($scope) {
 						$scope.$apply(function() {
 							objs.map(function(x) {
 								(x.id.indexOf('element') === 0 ? $scope.elements : $scope.constructs).push(x);
-							});						
-							$scope.element = $scope.elements[Math.floor($scope.elements.length*Math.random())];						
+							});
 							$scope.constructs = _($scope.constructs).sortBy(function(c) { return c.id; });
+							$scope.elements = $scope.elements.filter(function(el) { return done_element_ids.indexOf(el.id) < 0; });
+							console.log('scope elements ', $scope.elements);
+							if ($scope.elements.length > 0) {
+								//
+								console.debug('elements more than 0');
+								$scope.element = $scope.elements[Math.floor($scope.elements.length*Math.random())];
+								$scope.elicitation_obj.set({ 'expert' : $scope.username, 'element' : $scope.element.id });
+							}							
 						});
 					});
 					end_loading();
 				});					
-			});			
-		});		
+			});
+		}).fail(function () { show_error('Could not log in. Please check your webbox and try again.'); end_loading(); });
 	});
 }
